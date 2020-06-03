fe0083777e9a4b14fba03ad9e7394e92820c6ba1
Dhimas Dewanto <dhimasbrd@gmail.com>
2020-06-03 18:38:26 +0700

    Initiate random drink view

---
+++ lib/features/cocktails/presentation/fragments/random_fragment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Fragment to view random drink.
+class RandomFragment extends StatelessWidget {
+  const RandomFragment({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Random'),
+    );
+  }
+}
--- lib/features/cocktails/presentation/pages/cocktail_page.dart
+++ lib/features/cocktails/presentation/pages/cocktail_page.dart
@@ -1,9 +1,33 @@
 import 'package:cocktail_app/core/app_settings.dart';
+import 'package:cocktail_app/features/cocktails/presentation/fragments/random_fragment.dart';
 import 'package:flutter/material.dart';
 
 /// Homepage of Cocktail App.
-class CocktailPage extends StatelessWidget {
+class CocktailPage extends StatefulWidget {
   const CocktailPage({Key key}) : super(key: key);
+
+  @override
+  _CocktailPageState createState() => _CocktailPageState();
+}
+
+class _CocktailPageState extends State<CocktailPage> {
+  var _selectedIndex = 0;
+
+  final _listFragments = const [
+    RandomFragment(),
+    RandomFragment(),
+  ];
+
+  final _navbarItems = const [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.local_drink),
+      title: Text('Random'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.local_drink),
+      title: Text('Random'),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +35,18 @@ class CocktailPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(AppSettings.appName),
       ),
-      body: Container(),
+      body: _listFragments[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onFragmentChanged,
+        items: _navbarItems,
+      ),
     );
+  }
+
+  void _onFragmentChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
